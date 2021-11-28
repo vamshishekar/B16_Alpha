213a9f6b7b0d63d8ef58ae22196491a4dfd86dc1
vamshishekar <you@example.com>
2021-11-28 21:27:02 +0530

    fouth commit

--- B16ABookKart/src/app/app.module.ts
+++ B16ABookKart/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { KeyFilterPipe } from './pipe/key-filter.pipe';
 import { ProductFilterComponent } from './product-filter/product-filter.component';
+import { KeyFilterPipe } from './pipe/key-filter.pipe';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatListModule} from '@angular/material/list';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
-    KeyFilterPipe,
-    ProductFilterComponent
+    ProductFilterComponent,
+    KeyFilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule,HttpClientModule,FormsModule, BrowserAnimationsModule,
+    MatDividerModule,MatListModule,MatSliderModule,MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
